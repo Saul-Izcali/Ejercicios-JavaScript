519236d1cfa1bd2b7f2a0c1ccac8cadd73303846
Saul Izcali <saulizcali@gmail.com>
2021-04-07 20:34:30 -0500

    Menu hamburguesa terminado

---
+++ ArchivosJS/MenuHamburgesa.js
@@ -0,0 +1,33 @@
+
+
+export default function menuHamburgesa(boton){    
+    let activo = false;
+    //se esta utilizando delegacion de eventos
+    // se asigna el click a todo el documento de la pagina
+    // y si este click coincide con unos de los elementos que buscamos entonces se hace lo que queremos
+    document.addEventListener("click", (e) => {
+        // el if dice que si el elemento en el que se hizo el click es el boton del menu de una de estas 
+        // etiquetas a dentro del panel del menu entonces entra
+        // el metodo matches recibe como parametro cualquier elemento css valido
+        // `${boton} *` es para hacer que cualquier hijo del boton tambien funcione, en este caso los span
+        // que hacen las lineas
+        if(e.target.matches( boton) || e.target.matches( `${boton} *`) || e.target.matches("a")){
+            if(activo){
+                document.getElementById("panelMenu").className = "menuCerrado";
+                // document.getElementById("abrirMenu").className = "hamburger botonMenu is-active";
+                activo = false;
+            }else{
+                document.getElementById("panelMenu").className = "menuAbierto";
+                // document.getElementById("abrirMenu").className = "hamburger botonMenu";
+                activo = true;
+            }
+        }
+     });
+}
+
+
+// necesito checar bien la delegacion de eventos                        done
+// hacer que no se pierda la imagen del menu                            done
+// hacer que al presionar una seccion del menu este se cierre           done    
+// cambiar la imagen a una crucesita cuando se abre el menu
+
---
+++ ArchivosJS/index.js
@@ -0,0 +1,8 @@
+
+import menuHamburgesa from "./MenuHamburgesa.js";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    menuHamburgesa("span#boton.hamburger-box"); //llamamos a nuestra funcion menuhamburgesas que se encuntra en otro archivo
+                                                //con el parametro valido css del boton del menu
+});
+
