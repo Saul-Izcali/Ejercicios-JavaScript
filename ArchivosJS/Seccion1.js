966738c2370e09839c2b1519f68ecee87fa1225c
Saul Izcali <saulizcali@gmail.com>
2021-04-08 21:42:47 -0500

    Reloj digital terminado

---
+++ ArchivosJS/Seccion1.js
@@ -0,0 +1,67 @@
+
+let $tiempo = document.getElementById("contadorDeTiempo"), tiempoCorriendo = false, seg = 0, min = 0, hora = 0,
+    contador, alarma;
+
+
+export default function Seccion1(elemento){  
+
+    if(elemento.target.matches("button#iniciarReloj.botonSec1")){
+        $tiempo.style.marginTop = "40px";
+        $tiempo.style.marginBottom = "40px";
+        $tiempo.style.fontSize = "35px";
+
+        elemento.target.disabled = true;
+
+        contador = setInterval(() =>{
+            let clockHour = new Date().toLocaleTimeString();
+            $tiempo.textContent = clockHour;
+        }, 1000);
+
+        
+        // ------------ Este de abajo es un cronometro------------------
+
+        // contador = setInterval( function(){
+        //     if(seg < 10){
+        //         $tiempo.textContent = `0${hora}:0${min}:0${seg}`;
+        //     }else{
+        //         $tiempo.textContent = `0${hora}:0${min}:${seg}`;
+        //     }
+        //     seg++;
+        //     if(seg == 60){
+        //         min++;
+        //         seg = 0;
+        //     }
+        //     if(min == 60){
+        //         hora++;
+        //         min = 0;
+        //     }
+        // }, 1000);
+    }
+
+    if(elemento.target.matches("button#pararReloj.botonSec1")){
+        clearInterval(contador);
+    }
+
+    if(elemento.target.matches("button#iniciarAlarma.botonSec1")){
+        console.log("si")
+        const $elementoAlarma = document.createElement("audio");
+        $elementoAlarma.src = ".\ArchivosExtra/alarma.mp3";
+
+        alarma = setTimeout(() => {
+            $elementoAlarma.play();
+        }, 1000);
+
+        elemento.target.disabled = true;
+    }
+    
+    if(elemento.target.matches("button#pararAlarma.botonSec1")){
+        clearTimeout(alarma);
+        $elementoAlarma.pause();
+        $elementoAlarma.currentTime = 0;
+        elemento.target.disabled = true;
+    }
+
+}
+
+
+
--- ArchivosJS/index.js
+++ ArchivosJS/index.js
@@ -1,8 +1,22 @@
 
 import menuHamburgesa from "./MenuHamburgesa.js";
+import Seccion1 from "./Seccion1.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    menuHamburgesa("span#boton.hamburger-box"); //llamamos a nuestra funcion menuhamburgesas que se encuntra en otro archivo
+    menuHamburgesa("span#boton.hamburger-box"); //llamamos a nuestra funcion menuhamburgesas que se encuentra en otro archivo
                                                 //con el parametro valido css del boton del menu
 });
+
+document.addEventListener("click", (e) => {
+
+    if(e.target.matches(".botonSec1")){
+        Seccion1(e);
+    }
+
+
+});
+
+
+
+
 
